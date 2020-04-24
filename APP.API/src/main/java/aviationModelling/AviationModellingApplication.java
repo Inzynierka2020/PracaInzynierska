d514644776b875f38746344b9e756949770fac0e
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2020-04-24 23:01:10 +0200

    docker fix

--- APP.API/src/main/java/aviationModelling/AviationModellingApplication.java
+++ APP.API/src/main/java/aviationModelling/AviationModellingApplication.java
@@ -1,18 +1,22 @@
 package aviationModelling;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class AviationModellingApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(90000);
 		SpringApplication.run(AviationModellingApplication.class, args);
 	}
+
+
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer()
--- mssql/scripts/initialize.sh
+++ mssql/scripts/initialize.sh
@@ -1,7 +1,7 @@
 #wait for the SQL Server to come up
-echo "INITIALIZE 1";
+echo "INITIALIZING DATABASE";
 sleep 90s;
-echo "INITIALIZE 2";
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password@123 -i ./scripts/setup.sql;
+echo "DATABASE INITIALIZED";
 while :; do sleep 1; done;
