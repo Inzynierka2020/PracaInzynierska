e4539ada364b2fe532cbe7917fd71af8d1671020
Piotrek <piotrek.adamczykk@gmail.com>
2020-05-12 16:53:12 +0200

    Change token exp time

--- APP.API/src/main/java/aviationModelling/config/JwtTokenUtil.java
+++ APP.API/src/main/java/aviationModelling/config/JwtTokenUtil.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 public class JwtTokenUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
-    private static final long JWT_TOKEN_VALIDITY = 5;
+    private static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60;
 
     @Value("{jwt.secret}")
     private String secret;
