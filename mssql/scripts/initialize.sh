8827afe28cf8e10588adcdae8d2b334954c24ed2
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2020-09-19 11:32:14 +0200

    Container startup fix

--- mssql/scripts/initialize.sh
+++ mssql/scripts/initialize.sh
@@ -1,6 +1,6 @@
 #wait for the SQL Server to come up
 echo "INITIALIZING DATABASE";
-sleep 90s;
+sleep 30s;
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password@123 -i ./scripts/setup.sql;
 echo "DATABASE INITIALIZED";
