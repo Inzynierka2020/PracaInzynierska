83bcd3ec02757d35d09d22d5285ba327d3a0a1b4
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2020-10-19 03:29:46 +0200

    Retry

--- APP.UI/src/app/services/event.service.ts
+++ APP.UI/src/app/services/event.service.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Event } from '../models/event';
 import { IndexedDbService } from './indexed-db.service';
+import { Round } from '../models/round';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -43,9 +45,23 @@ export class EventService {
     return new Observable<boolean>(observer => {
       this._http.put(this._baseUrl + "events/total-score/" + eventId, null).subscribe(
         result => {
-          //PUT ALL -> 
-          //RETRY
-          observer.next(true);
+          if (this._dbService.hasPriority()) {
+            this.updateAllRoundsFromDB(eventId).pipe(take(1)).subscribe(
+              result => {
+                if (result) {
+                  this._dbService.setPriority(false);
+                  this.updateGeneralScore(eventId).pipe(take(1)).subscribe(result => {
+                    observer.next(result);
+                  })
+                } else {
+                  this._dbService.setPriority(true);
+                  observer.next(false);
+                }
+              }
+            )
+          } else {
+            observer.next(true);
+          }
         },
         error => {
           this._dbService.setPriority(true);
@@ -53,5 +69,21 @@ export class EventService {
         }
       )
     })
+  }
+
+  updateAllRoundsFromDB(eventId: number): Observable<boolean> {
+    return new Observable<boolean>(observer => {
+      this._dbService.readAllRounds(eventId).pipe(take(1)).subscribe(result => {
+        this._http.put(this._baseUrl + "rounds/synchronize-all/", result).pipe(take(1)).subscribe(
+          result => {
+            observer.next(true);
+          },
+          error => {
+            this._dbService.setPriority(true);
+            observer.next(false);
+          }
+        )
+      })
+    });
   }
 }
