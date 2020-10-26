395212955f847cf2512e3fdc07db7a3979a09846
Piotr Adamczyk <piotr.adamczyk@inero.pl>
2020-10-26 19:03:51 +0100

    Add registation form validation

--- APP.API/src/main/java/aviationModelling/dto/UserDTO.java
+++ APP.API/src/main/java/aviationModelling/dto/UserDTO.java
@@ -1,18 +1,30 @@
 package aviationModelling.dto;
 
+import aviationModelling.validators.UniqueUsername;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
 @Setter
 public class UserDTO {
 
+    @UniqueUsername
+    @NotBlank
     private String username;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
+
+
     private String vaultLogin;
     private String vaultPassword;
     private String vaultUrl;
