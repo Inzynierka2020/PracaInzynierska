3914a13812c120a92ad9a2b75aa9a9d4d569c392
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2020-10-28 13:18:57 +0100

    db fix

--- mssql/scripts/initialize.sh
+++ mssql/scripts/initialize.sh
@@ -2,6 +2,6 @@
 echo "INITIALIZING DATABASE";
 sleep 30s;
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password@123 -i ./scripts/setup.sql;
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Password@123 -i ./scripts/setup.sql;
 echo "DATABASE INITIALIZED";
 while :; do sleep 1; done;
