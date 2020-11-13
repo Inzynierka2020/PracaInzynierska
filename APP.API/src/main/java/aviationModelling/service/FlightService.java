b48eb4a9a6b08118cd61ed7dbc88d91e44446f4f
Piotr Adamczyk <piotr.adamczyk@inero.pl>
2020-11-13 20:57:49 +0100

    Fix the vault bug with updating finished round flag

--- APP.API/src/main/java/aviationModelling/service/FlightService.java
+++ APP.API/src/main/java/aviationModelling/service/FlightService.java
@@ -12,8 +12,6 @@ public interface FlightService {
     ResponseEntity<VaultResponseDTO> postScore(Integer roundNum, Integer pilotId, Integer eventId);
     ResponseEntity<?> delete(FlightDTO flightDTO);
     Flight findFlight(Integer roundNum, Integer pilotId, Integer eventId);
-//    ResponseEntity<List<FlightDTO>> saveAll(List<FlightDTO> flightList);
-//    FlightDTO findBestTime(Integer eventId);
-//
+    Integer findHighestValidRoundNumber(Integer eventId);
 
 }
